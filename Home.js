49d0f571da502524bf6ecb9e3353fdf63367df85
BadhanDatta <hitleradlof72@gmail.com>
2025-04-15 13:26:01 +0100

    Update Home.js

--- Home.js
+++ Home.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const getStartedBtn = document.getElementById("getStartedBtn");
   
     getStartedBtn.addEventListener("click", function () {
-      window.location.href = "product.html"; // Redirects to the products page
+      window.location.href = "product.html"; 
     });
   });
   
